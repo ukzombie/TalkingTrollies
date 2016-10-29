03854a1e20d5b44d1cc537122f147fce8e381f2d
Brandon Conley <brandon.conley@redeye.com>
2016-10-29 15:41:46 +0000

    Default espeak voices (Four Female, 7 Male)

---
+++ EspeakCommands.py
@@ -0,0 +1,17 @@
+import os
+#os.system("espeak 'Hello'")
+#Woman voice
+os.system("espeak -ven+f1 -k5 -s150 'Hello'")
+os.system("espeak -ven+f2 -k5 -s150 'Hello'")
+os.system("espeak -ven+f3 -k5 -s150 'Hello'")
+os.system("espeak -ven+f4 -k5 -s150 'Hello'")
+
+#Mens Voice
+os.system("espeak -ven+m1 -k5 -s150 'Hello'")
+os.system("espeak -ven+m2 -k5 -s150 'Hello'")
+os.system("espeak -ven+m3 -k5 -s150 'Hello'")
+os.system("espeak -ven+m4 -k5 -s150 'Hello'")
+os.system("espeak -ven+m5 -k5 -s150 'Hello'")
+os.system("espeak -ven+m6 -k5 -s150 'Hello'")
+os.system("espeak -ven+m7 -k5 -s150 'Hello'")
+
