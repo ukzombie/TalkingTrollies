5d0c69ff8f6307a22d2c69527489ddf7d5bb6784
Brandon Conley <brandon.conley@redeye.com>
2016-10-29 16:32:20 +0000

    Added two arrays of speach for 2 baskets

--- EspeakCommands.py
+++ EspeakCommands.py
@@ -1,4 +1,10 @@
 import os
+from array import *
+
+basket_a_speak = ['Chocolate comes from cocoa, which is a tree. That makes it a plant. Therefore, chocolate is salad.','Damn, I forgot to go to the gym today. Thats 10 years in a row now...']
+
+basket_b_speak = ['Im learning the hokey cokey. Not all of it. But  Ive got the ins and outs.','Ive got very sensitive teeth. Theyll probably be upset Ive told you.']
+
 #os.system("espeak 'Hello'")
 #Woman voice
 os.system("espeak -ven+f1 -k5 -s150 'Hello'")
