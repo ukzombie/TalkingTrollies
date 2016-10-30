aff7776ad0c2a765bcff929273b96b9a8a108d15
Brandon Conley <brandon.conley@redeye.com>
2016-10-30 00:49:44 +0000

    Testing different voice dialects

--- EspeakCommands.py
+++ EspeakCommands.py
@@ -1,23 +1,43 @@
 import os
-from array import *
+#from array import *
 
-basket_a_speak = ['Chocolate comes from cocoa, which is a tree. That makes it a plant. Therefore, chocolate is salad.','Damn, I forgot to go to the gym today. Thats 10 years in a row now...']
+#basket_a_speak = ['Chocolate comes from cocoa, which is a tree. That makes it a plant. Therefore, chocolate is salad.','Damn, I forgot to go to the gym today. Thats 10 years in a row now...']
 
-basket_b_speak = ['Im learning the hokey cokey. Not all of it. But  Ive got the ins and outs.','Ive got very sensitive teeth. Theyll probably be upset Ive told you.']
+#basket_b_speak = ['Im learning the hokey cokey. Not all of it. But  Ive got the ins and outs.','Ive got very sensitive teeth. Theyll probably be upset Ive told you.']
+
+#print basket_a_speak[1]
 
 #os.system("espeak 'Hello'")
 #Woman voice
-os.system("espeak -ven+f1 -k5 -s150 'Hello'")
-os.system("espeak -ven+f2 -k5 -s150 'Hello'")
-os.system("espeak -ven+f3 -k5 -s150 'Hello'")
-os.system("espeak -ven+f4 -k5 -s150 'Hello'")
+#os.system("espeak -ven+f1 -k5 -s150 'Chocolate comes from cocoa, which is a tree. That makes it a plant. Therefore, chocolate is salad.'")
+#os.system("espeak -ven+f2 -k5 -s150 'Chocolate comes from cocoa, which is a tree. That makes it a plant. Therefore, chocolate is salad.'")
+#os.system("espeak -ven-uk-wmids+f2 -k5 -s200 'Chocolate coomes from cowcaw, which is a tree. Tha makes it a plant. So, chocolate is salad.'")
+#os.system("espeak -ven-us+f2 -k5 -s160 'Chocolate coomes from coocoo, which is a tree. Tha makes it a plant. So, chocolate is salad.'")
+#os.system("espeak -ven+f3 -k5 -s150 'Chocolate comes from cocoa, which is a tree. That makes it a plant. Therefore, chocolate is salad.'")
+#os.system("espeak -ven+f4 -k5 -s150 'Chocolate comes from cocoa, which is a tree. That makes it a plant. Therefore, chocolate is salad.'")
+
+#os.system("espeak -ven-sc+f2 -p70 -k5 -s180 'Have you heard about the oyster, who went into a disco and pulled a muscle'") #Winning Lady
+#os.system("espeak -ven-sc+f2 -p70 -k5 -s180 'Och! Damn, I forgot to go to the gym today. Thatll be 10 years in a row now...'") #Final slight issues
+os.system("espeak -ven-sc+f2 -p70 -k5 -s180 'Oh Hello'")
+os.system("espeak -ven-sc+f2 -p70 -k5 -s180 'Hi There'")
+
+
 
 #Mens Voice
-os.system("espeak -ven+m1 -k5 -s150 'Hello'")
-os.system("espeak -ven+m2 -k5 -s150 'Hello'")
-os.system("espeak -ven+m3 -k5 -s150 'Hello'")
-os.system("espeak -ven+m4 -k5 -s150 'Hello'")
-os.system("espeak -ven+m5 -k5 -s150 'Hello'")
-os.system("espeak -ven+m6 -k5 -s150 'Hello'")
-os.system("espeak -ven+m7 -k5 -s150 'Hello'")
+#os.system("espeak -ven-wi+m1 -k5 -s150 'Ive got very sensitive teeth. Theyll probably be upset Ive told you.'")
+#os.system("espeak -ven-wi+m2 -k5 -s150 'Ive got very sensitive teeth. Theyll probably be upset Ive told you.'")
+#os.system("espeak -ven-wi+m3 -k5 -s150 'Ive got very sensitive teeth. Theyll probably be upset Ive told you.'")
+#os.system("espeak -ven-wi+m4 -k5 -s150 'Ive got very sensitive teeth. Theyll probably be upset Ive told you.'")
+#os.system("espeak -ven-wi+m5 -k5 -s150 'Ive got very sensitive teeth. Theyll probably be upset Ive told you.'")
+#os.system("espeak -ven-wi+m6 -k5 -s150 'Ive got very sensitive teeth. Theyll probably be upset Ive told you.'")
 
+os.system("espeak -ven-wi+m7 -k5 -s150 'Ive got very sensitive teeth. Theyll probably be upset Ive told you.'")         #WestIndies Winner
+os.system("espeak -ven-wi+m7 -k5 -s150 'I am learning the hokey cokey. Not all of it. But  Ive got the ins and outs.'") #WestIndies Winner
+
+
+#en-sc scottish
+#en-uk-north northern
+#en-uk-wmids
+#en-uk-rp
+#en-us
+#en-wi 
